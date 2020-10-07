f98c6e599fedec9e0996cbbf801790301c76ad1c
fengchen <chen.feng1@husky.neu.edu>
2020-10-07 00:16:00 -0700

    add server

---
+++ Assignment1/src/main/java/Server/SkierServlet.java
@@ -0,0 +1,89 @@
+package Server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "SkierServlet")
+public class SkierServlet extends HttpServlet {
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse res)
+      throws ServletException, IOException {
+    res.setContentType("application/json");
+    String urlPath = req.getPathInfo();
+    System.out.println(urlPath);
+    // check we have a URL!
+    if (urlPath == null || urlPath.isEmpty()) {
+      res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      res.getWriter().write("missing paramterers");
+      return;
+    }
+
+    String[] urlParts = urlPath.split("/");
+
+
+    if (!isUrlValid(urlParts)) {
+      res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } else {
+      res.setStatus(HttpServletResponse.SC_OK);
+      // do any sophisticated processing with urlParts which contains all the url params
+
+      //res.getWriter().write("It works!");
+    }
+  }
+
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    res.setContentType("application/json");
+    String urlPath = req.getPathInfo();
+    System.out.println(urlPath);
+    // check we have a URL!
+    if (urlPath == null || urlPath.isEmpty()) {
+      res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      res.getWriter().write("missing paramterers");
+      return;
+    }
+
+    String[] urlParts = urlPath.split("/");
+    // and now validate url path and return the response status code
+    // (and maybe also some value if input is valid)
+
+    if (!isUrlValid(urlParts)) {
+      res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } else {
+      res.setStatus(HttpServletResponse.SC_OK);
+
+    }
+  }
+
+  private boolean isNumber(String s) {
+    for (char c : s.toCharArray()) {
+      if (!Character.isDigit(c)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean isUrlValid(String[] urlPath) {
+    // urlPath  = "/1/seasons/2019/day/1/skier/123"
+    // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
+
+    if (urlPath.length == 8
+        && isNumber(urlPath[1])
+        && isNumber(urlPath[3])
+        && isNumber(urlPath[5])
+        && isNumber(urlPath[7])
+        && urlPath[2].equals("seasons")
+        && urlPath[4].equals("days")
+        && urlPath[6].equals("skiers")
+        ) {
+      return true;
+    }
+
+    return true;
+  }
+}
